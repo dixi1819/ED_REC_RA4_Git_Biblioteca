d83913a3dd68cf88d68d53c2effc8b64a5ec60c8
dixi1819 <dixiana1291@hotmail.com>
2026-06-01 18:50:07 +0200

    feat: añade menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Biblioteca iniciada correctamente.");
+        System.out.println("1. Añadir libro");
+        System.out.println("2. Listar libros");
+        System.out.println("3. Salir");
     }
 }
